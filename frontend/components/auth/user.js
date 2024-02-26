0bf8121d0b125ff06586726cc312abe00d8b4c15
laveshpal <laveshpal@gmail.com>
2024-02-26 13:57:12 +0530

    updated code

--- frontend/components/auth/user.js
+++ frontend/components/auth/user.js
@@ -1,35 +1,34 @@
 import axios from "axios";
-import cookie from 'cookie'
+import cookie from "cookie";
+
 export default async (req, res) => {
+    if (req.method === "GET") {
+        const cookies = cookie.parse(req.headers.cookie || "");
 
-    if (req.method === 'GET') {
+        const access = cookies.access || false;
 
-        const cookies = cookie.parse(req.headers.cookie || '')
-
-        const access = cookies.access || false
-
-        if(!access){
+        if (!access) {
             return res.status(401).json({
-                error: 'Login first to load user'
-            })
+                message: "Login first to load user",
+            });
         }
 
-
         try {
+            const response = await axios.get(`${process.env.API_URL}/api/me/`, {
+                headers: {
+                    Authorization: `Bearer ${access}`,
+                },
+            });
 
-            const response = await axios.get(`${process.env.API_URL}/api/me/`,
-                {headers: {'Authorization': `Bearer ${access}`,},});
-
-            if(response.data){
+            if (response.data) {
                 return res.status(200).json({
-                    user: response.data
-                })
+                    user: response.data,
+                });
             }
-
-
         } catch (error) {
-            res.status(error?.response.status).json({error: 'Somthing went wrong while retrieving user'})
-
+            res.status(error?.response.status).json({
+                error: "Something went wrong while retrieving user",
+            });
         }
     }
 };
